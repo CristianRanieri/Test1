a3a34a836c145278bd445bba1088209ff1cc0cc5
Cristian Ranieri <C.RANIERI7@STUDENTI.UNISA.IT>
2023-09-24 15:04:02 +0200

    first commit fot test

--- src/main/java/com/example/testgithub/HelloServlet.java
+++ src/main/java/com/example/testgithub/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World! ";
+        message = "Cazzi amari ";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
